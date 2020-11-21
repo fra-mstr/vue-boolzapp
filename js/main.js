ec5fa92f1d23ce0c219018093772dd0c8dd88f95
Francesco Maestroni <francescomaestroni1@gmail.com>
2020-11-21 11:44:07 +0100

    Layout Parte sx e prima implementazione Vue

--- js/main.js
+++ js/main.js
@@ -1,11 +1,27 @@
-const boolzapp = new Vue ({
-  el: #boolzapp,
+const myApp = new Vue ({
+  el: '#root',
   data: {
-    contactList = [
+    contactArr: [
       {
-        name: 'Michele',
-        avatar: 'img/avatar_1.jpg'
+        'name': 'Michele',
+        'avatar': 'img/avatar_1.jpg',
+        'lastAccess': '01.17'
       },
+      {
+        'name': 'Fabio',
+        'avatar': 'img/avatar_2.jpg',
+        'lastAccess': '11.08'
+      },
+      {
+        'name': 'Samuele',
+        'avatar': 'img/avatar_3.jpg',
+        'lastAccess': '18.40'
+      },
+      {
+        'name': 'Luisa',
+        'avatar': 'img/avatar_4.jpg',
+        'lastAccess': '21.28'
+      }
     ]
   }
 })
